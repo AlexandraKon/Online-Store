8c1609b7a10042f69df6b3ee5590737136f0c803
Alexandra Kon <kononenkoalexandra2002@gmail.com>
2022-08-26 13:16:23 +0200

    add changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
-import React from 'react';
-import { Homepage } from "../src/components/Homepage/Homepage";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Route, Routes } from 'react-router-dom';
+import { Homepage } from "./pages/Homepage";
+import { AuthContextProvider} from './context/AuthContext';
+import { Nav } from './components/Nav/Nav';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+
 
 function App() {
     return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<Homepage />} />
-            </Routes>
-        </Router>
+        <>
+            <AuthContextProvider>
+                <Nav/>
+                <Routes>
+                    <Route path='/' element={<Homepage />} />
+                    <Route path='/login' element={<Login />} />
+                    <Route path='/signup' element={<Signup />} />
+                </Routes>
+            </AuthContextProvider>
+            
+        </>
     );
 }
 
---
+++ src/context/AuthContext.ts
@@ -0,0 +1,13 @@
+import { createContext, useContext, useEffect, useState } from 'react';
+
+const AuthContext = createContext();
+
+export function AuthContextProvider( ) {
+    const [ user, setUser] = useState({});
+
+
+    return (
+       
+    );
+
+};
